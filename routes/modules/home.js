62187a2b332b2681d1e30376b150f92cadb40fcb
muco0521 <muco0521@gmail.com>
2023-07-11 18:12:20 +0800

    feat: add home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,7 +1,15 @@
 const router = require('express').Router()
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
 
 router.get('/', (req, res) => {
-  res.send('hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => res.render('index', { todos }))
+    .catch(error => console.error(error))
 })
 
 module.exports = router
