bebb3c076e1b75c176785f7503682887f9e10c1b
muco0521 <muco0521@gmail.com>
2023-07-11 19:37:10 +0800

    feat: add detail page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const users = require('./modules/users')
 const home = require('./modules/home')
+const todos = require('./modules/todos')
 
+router.use('/todos', todos)
 router.use('/users', users)
 router.use('/', home)
 
---
+++ routes/modules/todos.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then((todo) => res.render('detail', { todo: todo.toJSON() }))
+    .catch((e) => console.log(e))
+})
+
+module.exports = router
