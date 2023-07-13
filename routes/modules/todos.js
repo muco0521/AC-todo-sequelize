8bc88d01872927c4f7797c3cd1792240143926ba
muco0521 <muco0521@gmail.com>
2023-07-13 22:50:38 +0800

    feat: add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -1,12 +1,23 @@
 const router = require('express').Router()
 const db = require('../../models')
 const Todo = db.Todo
-const User = db.User
+
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
 
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then((todo) => res.render('detail', { todo: todo.toJSON() }))
+    .catch((e) => console.log(e))
+})
+
+router.post('/', (req, res) => {
+  const name = req.body.name
+  const UserId = req.user.id
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
     .catch((e) => console.log(e))
 })
 
